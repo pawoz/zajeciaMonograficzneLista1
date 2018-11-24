beea4ddddb1c0b5ee0c281e26350db5b7eac0507
Paweł Woźny <pawel.wozny@op.pl>
2018-11-24 12:22:41 +0100

    third commit

--- src/main/java/lista1/losowanie/Losowanie1.java
+++ src/main/java/lista1/losowanie/Losowanie1.java
@@ -1,16 +1,23 @@
 package lista1.losowanie;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Losowanie1 {
 
     public static void main(String[] args) {
 
+        int min;
+        int max;
         int randomNumber;
         Random random = new Random();
+        Scanner in = new Scanner(System.in);
+        System.out.println("podaj początek zakresu");
+        min = in.nextInt();
+        System.out.println("podaj koniec zakresu");
+        max = in.nextInt();
 
-        randomNumber = random.nextInt(49) + 1;
-
+        randomNumber = random.nextInt(max) + min;
         System.out.println(randomNumber);
     }
 }
