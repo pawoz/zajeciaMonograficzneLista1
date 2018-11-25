6dc2bca4f3e5a9629aecbbfae6fba6fb596c80a0
Paweł Woźny <pawel.wozny@op.pl>
2018-11-25 10:14:20 +0100

    losowanie5

--- src/main/java/lista1/losowanie/Losowanie5.java
+++ src/main/java/lista1/losowanie/Losowanie5.java
@@ -1,26 +1,59 @@
 package lista1.losowanie;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Losowanie5 {
 
-    private static List<Integer> numbers = new ArrayList<Integer>();
+    private final static int DAYS_IN_YEAR = 365;
+    private final static int PROBABILITY = 13983816;
+    private final static int MIN = 1;
+    private final static int MAX = 49;
+    private static int iterations = 0;
+
+    private static TreeSet<Integer> userNumbers = new TreeSet<Integer>();
+    private static TreeSet<Integer> computerNumbers = new TreeSet<Integer>();
+
+    public static void computerDrawn() {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        for (int i = 0; computerNumbers.size() < 6; i++) {
+            computerNumbers.add(random.nextInt(MAX) + MIN);
+        }
+        iterations++;
+    }
+
+    public static void compareNumbers() {
+        while (!(computerNumbers.containsAll(userNumbers))) {
+            computerNumbers.clear();
+            computerDrawn();
+        }
+    }
 
     public static void main(String[] args) {
-
         Random random = new Random();
+        Scanner in = new Scanner(System.in);
+        System.out.println();
 
-        for (int i = 0; i < 6; i++) {
-            numbers.add(random.nextInt(49) + 1);
+        for (int i = 0; userNumbers.size() < 6; i++) {
+            System.out.println("podaj liczbę z przedziału od " + MIN + " do " + MAX);
+            int givenNumber = in.nextInt();
+            if (givenNumber >= MIN && givenNumber <= MAX) {
+                userNumbers.add(givenNumber);
+            } else {
+                System.out.println("podałeś liczbę spoza przedziału");
+            }
         }
 
-        Collections.sort(numbers);
-
-        for (int x : numbers) {
+        computerDrawn();
+        compareNumbers();
+        System.out.println("Liczby podane przez użytkownika");
+        for (int x : userNumbers)
             System.out.println(x);
-        }
+        System.out.println("Liczby wylosowane przez ukomputer");
+        for (int x : computerNumbers)
+            System.out.println(x);
+        System.out.println("Komputer wylosował twoje liczby po " + iterations + " losowaniach, co daje " + iterations / DAYS_IN_YEAR + " lat.");
     }
 }
