308cb5645bc9666c31fffed2ab835b1f7984a9ed
Paweł Woźny <pawel.wozny@op.pl>
2018-12-08 12:08:39 +0100

    po cr

--- src/main/java/lista1/losowanie/Losowanie5.java
+++ src/main/java/lista1/losowanie/Losowanie5.java
@@ -15,6 +15,24 @@ public class Losowanie5 {
     private static TreeSet<Integer> userNumbers = new TreeSet<Integer>();
     private static TreeSet<Integer> computerNumbers = new TreeSet<Integer>();
 
+    public static void userNumbers() {
+
+        Random random = new Random();
+        Scanner in = new Scanner(System.in);
+
+
+        for (int i = 0; userNumbers.size() < 6; i++) {
+            System.out.println("podaj liczbę z przedziału od " + MIN + " do " + MAX);
+            int givenNumber = in.nextInt();
+            if (givenNumber >= MIN && givenNumber <= MAX) {
+                userNumbers.add(givenNumber);
+            } else {
+                System.out.println("podałeś liczbę spoza przedziału");
+            }
+        }
+        System.out.println("Computer is drawing...");
+    }
+
     public static void computerDrawn() {
         Random random = new Random();
         for (int i = 0; computerNumbers.size() < 6; i++) {
@@ -30,29 +48,27 @@ public class Losowanie5 {
         }
     }
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        Scanner in = new Scanner(System.in);
-        System.out.println();
-
-        for (int i = 0; userNumbers.size() < 6; i++) {
-            System.out.println("podaj liczbę z przedziału od " + MIN + " do " + MAX);
-            int givenNumber = in.nextInt();
-            if (givenNumber >= MIN && givenNumber <= MAX) {
-                userNumbers.add(givenNumber);
-            } else {
-                System.out.println("podałeś liczbę spoza przedziału");
-            }
-        }
-
-        computerDrawn();
-        compareNumbers();
+    public static void printUserNumbers() {
         System.out.println("Liczby podane przez użytkownika");
         for (int x : userNumbers)
             System.out.println(x);
+    }
+
+    public static void printComputerNumbers() {
+
         System.out.println("Liczby wylosowane przez ukomputer");
         for (int x : computerNumbers)
             System.out.println(x);
         System.out.println("Komputer wylosował twoje liczby po " + iterations + " losowaniach, co daje " + iterations / DAYS_IN_YEAR + " lat.");
+
+    }
+
+    public static void main(String[] args) {
+
+        userNumbers();
+        computerDrawn();
+        compareNumbers();
+        printUserNumbers();
+        printComputerNumbers();
     }
 }
